00dffcf1b2ff2ebb8a9e1b91d42ccdfe539886be
Ellie Phant <itsphant@gmail.com>
2017-11-14 22:48:46 -0500

    Removed README and startup script

--- bot/y-bot-flask-rest.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-clear
-
-export PYTHONPATH=./src:../program-y/src:../bot-chan/libs/MetOffer-1.3.2:.
-
-python3 ../program-y/src/programy/clients/flaskrest.py --config ./config.yaml --cformat yaml --logging ./logging.yaml
-
